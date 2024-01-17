454d39f2302c64ca1f45493ee5aa2d6735b6dc93
Sumeya <sumeyaibrahim72@gmail.com>
2024-01-17 16:09:23 +0300

    Initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      hellow world
-    </div>
-  );
+    return <div className="App bg-red-300">hellow world</div>;
 }
 
 export default App;
